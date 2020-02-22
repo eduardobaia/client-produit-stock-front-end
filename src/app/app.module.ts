c60f682a7aadc605366a21a53f78d020ded76308
eduardobaia <edubaia10>
2020-02-22 17:12:41 +0000

    ad sidebar, navbar and content components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
+import { ProduitMockService } from './produit/produit.mock.service';
+import { ProduitModule } from './shared/produit/produit.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProduitComponent } from './produit/produit.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProduitComponent,
+    NavbarComponent,
+    SidebarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ProduitModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/produit/produit.component.ts
@@ -0,0 +1,24 @@
+import { ProduitMockService } from './produit.mock.service';
+
+import { Component, OnInit } from '@angular/core';
+import { ProduitModule } from '../shared/produit/produit.module';
+
+
+@Component({
+  selector: 'app-produit',
+  templateUrl: './produit.component.html',
+  styleUrls: ['./produit.component.css']
+})
+export class ProduitComponent implements OnInit {
+
+  produits : ProduitModule[];
+
+  constructor(private  productService:ProduitMockService) { 
+
+  }
+
+  ngOnInit() {
+    this.produits = this.productService.getProduits();
+  }
+
+}
